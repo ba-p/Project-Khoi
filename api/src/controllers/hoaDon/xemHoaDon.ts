f2ad161cf7949f65923a01ee5622bdba802fc8ba
khangb1910652 <93603484+khangb1910652@users.noreply.github.com>
2021-11-27 22:05:20 +0700

    khang

---
+++ api/src/controllers/hoaDon/xemHoaDon.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { connection } from "../../database/mysql";
+
+const router = express.Router();
+
+export const xemHoaDon = () => {
+  return router.get(
+    "/",
+    async (req: express.Request, res: express.Response) => {
+      try {
+        
+        const sql = "call xemhoadon())";
+        connection.query(sql, (err, result) => {
+          if (err) throw err;
+          res.json(result[0]);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+};
