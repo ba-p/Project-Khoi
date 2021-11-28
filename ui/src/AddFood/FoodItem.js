9475435b55cf7b04e88589ecd0a63f8cdbb7649f
Phuong Tran <phuong.tran@ptnglobalcorp.com>
2021-11-28 20:37:26 +0700

    a

--- ui/src/AddFood/FoodItem.js
+++ ui/src/AddFood/FoodItem.js
@@ -1,20 +1,32 @@
 import { useState } from "react";
 
-const FoodItem = ({item}) => {
-    const [statusCol, setstatusCol] = useState({ 
-        col1: 'normal', col2: 'normal', col3: 'normal'
-    })
-    return ( 
-        <tr>
-            {/* {col1==='normal'? */}
-            <td style={{verticalAlign: 'middle', textAlign: 'center'}} onDoubleClick={()=>{alert("Double click")}}>{item.id}</td>
-            {/* <input>{item.idmonan}</input> */}
-            {/* } */}
-            
-            <td style={{verticalAlign: 'middle', textAlign: 'center'}}>{item.tenMon}</td>
-            <td style={{verticalAlign: 'middle', textAlign: 'center'}}>{item.gia}</td>
-        </tr>
-     );
-}
- 
+const FoodItem = ({ item }) => {
+  const [isEditId, setEditId] = useState(false);
+
+  return (
+    <tr>
+      {/* {col1==='normal'? */}
+      {!isEditId ? (
+        <td
+          style={{ verticalAlign: "middle", textAlign: "center" }}
+          onDoubleClick={() => {
+            setEditId(true);
+          }}
+        >
+          {item.id}
+        </td>
+      ) : (
+        <input value={item.id} />
+      )}
+
+      <td style={{ verticalAlign: "middle", textAlign: "center" }}>
+        {item.tenMon}
+      </td>
+      <td style={{ verticalAlign: "middle", textAlign: "center" }}>
+        {item.gia}
+      </td>
+    </tr>
+  );
+};
+
 export default FoodItem;
--- ui/src/Voucher/InVoucher.js
+++ ui/src/Voucher/InVoucher.js
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 export const InVoucher = (props) => {
-    const { item } = props;
+  const { item, setValue } = props;
   return (
     <tr style={{ backgroundColor: "#fff", height: "30px" }}>
-      
-        <th>{item?.idvoucher}</th>
-        <th>{item.giamgia}</th>
-        
+      <th>
+        {item?.idvoucher ? (
+          item.idvoucher
+        ) : (
+          <input
+            placeholder="id của voucher"
+            onChange={(e) =>
+              setTimeout(() => setValue("id", e.target.value), 1000)
+            }
+          />
+        )}
+      </th>
+      <th>
+        {item?.giamgia > 0 ? (
+          item?.giamgia
+        ) : (
+          <input
+            placeholder="số tiền giảm"
+            onChange={(e) =>
+              setTimeout(() => setValue("price", e.target.value), 1000)
+            }
+          />
+        )}
+      </th>
     </tr>
   );
 };
